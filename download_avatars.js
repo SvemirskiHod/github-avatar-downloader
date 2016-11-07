16757fbcb1d7d2ffd0dde4c8989bbe702144b5a5
Your name <jaocoelho93@gmail.com>
2016-11-07 22:40:55 +0000

    successfuly grabbing contributors of projects

--- download_avatars.js
+++ download_avatars.js
@@ -1,2 +1,29 @@
 var request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader');
+
+
+
+function getRepoContributors(repoOwner, repoName, cb){
+
+var GITHUB_USER = "SvemirskiHod";
+var GITHUB_TOKEN = "f9e45c29a26822f62459d5a506e6b091fce71bd8";
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+console.log(requestURL);
+
+  request({url: requestURL,
+           headers: {'User-Agent': 'request'},
+          },
+         function(err, response, body) {
+    cb(err, JSON.parse(body));
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
+  // auth: {
+           //  'user': 'SvemirskiHod',
+           //  'pass': 'f9e45c29a26822f62459d5a506e6b091fce71bd8',
+           //  }
