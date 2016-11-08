5de30eb793fc8eefa55b41b71f726431eb9c7f78
Your name <jaocoelho93@gmail.com>
2016-11-08 20:00:53 +0000

    Made required changes for assignment Re-Submit

--- download_avatars.js
+++ download_avatars.js
@@ -1,43 +1,48 @@
-var request = require('request');
-var token = require("../token.js"); //requiring token as to not push token to gitHub
-var fs = require('fs');
+require('dotenv').config();
+const request = require('request');
+const token = process.env.token;
+const userName = process.env.userName; //requiring token as to not push token to gitHub
+const fs = require('fs');
 console.log('Welcome to the GitHub Avatar Downloader');
-var input = process.argv.slice(2);
+const input = process.argv.slice(2);
 
-if(input.length !== 2){ //will give error if number of arguments is not equal to 2
-  console.log("Error! Specify two arguments: Repo Owner and the Repo Name, in that order");
-}
-else {
-  getRepoContributors(input[0],input[1], function(err, result) { // This runs the program by calling the functions
-    result.forEach(function(elm){
-      var avatarUrl = elm["avatar_url"];
-      var login = elm["login"];
-      var path = "./avatars/" + login + ".jpg";
-      downloadImageByUrl(avatarUrl, path);
-    })
-  });
+const createUrl = function (userName, token, repoOwner, repoName){
+  return requestURL = `https://${userName}:${token}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
 }
 
-function getRepoContributors(repoOwner, repoName, cb){
-  var GITHUB_USER = "SvemirskiHod";
-  var GITHUB_TOKEN = token.token; // implementing token from separate file
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+const downloadImageByUrl = function (url, filePath){
+  request.get(url)
+  .pipe(fs.createWriteStream(filePath));
+}
+
+const getRepoContributors = function (cb){
   request({
-    url: requestURL,
+    url: createUrl(userName, token, input[0],input[1]),
     headers: {'User-Agent': 'request'},
   },
   function(err, response, body) {
     if(err) {
       console.log(err);
     } else {
-      var data = !!body ? JSON.parse(body) : []; // Mentor did this when we were trying to debug something. Probably not necessary, but I kept it.
-      cb(err, data);
+      cb(err, JSON.parse(body));
     }
   }
   );
 }
 
-function downloadImageByUrl(url, filePath){
-  request.get(url)
-  .pipe(fs.createWriteStream(filePath));
+if(input.length !== 2){ //will give error if number of arguments is not equal to 2
+  console.log("Error! Specify two arguments: Repo Owner and the Repo Name, in that order");
 }
+else {
+  getRepoContributors(function(err, result) { // This runs the program by calling the functions
+    result.forEach(function(elm){
+      const avatarUrl = elm["avatar_url"];
+      const login = elm["login"];
+      const path = "./avatars/" + login + ".jpg";
+      downloadImageByUrl(avatarUrl, path);
+    })
+  });
+}
+
+
+
