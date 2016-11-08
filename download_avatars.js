54cb8a21d5631407f11b29b1076112923c872cb5
Your name <jaocoelho93@gmail.com>
2016-11-08 00:54:50 +0000

    App now downloads contributor images from a git repo. It names the fales accroding to the login name

--- download_avatars.js
+++ download_avatars.js
@@ -1,26 +1,40 @@
 var request = require('request');
-var token = require("../token.js");
+var token = require("../token.js"); //requiring token as to not push token to gitHub
+var fs = require('fs');
 console.log('Welcome to the GitHub Avatar Downloader');
-var require
-
 
 function getRepoContributors(repoOwner, repoName, cb){
-
-var GITHUB_USER = "SvemirskiHod";
-var GITHUB_TOKEN = token.token;
-var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-
-  request({url: requestURL,
-           headers: {'User-Agent': 'request'},
-          },
-         function(err, response, body) {
-    cb(err, JSON.parse(body));
-  });
+  var GITHUB_USER = "SvemirskiHod";
+  var GITHUB_TOKEN = token.token;
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  request({
+    url: requestURL,
+    headers: {'User-Agent': 'request'},
+  },
+  function(err, response, body) {
+    if(err) {
+      console.log(err);
+    } else {
+      var data = !!body ? JSON.parse(body) : [];
+      cb(err, data);
+    }
+  }
+  );
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
   result.forEach(function(elm){
-    console.log(elm['avatar_url'])
-  });
+    var avatarUrl = elm["avatar_url"];
+    var login = elm["login"];
+    var path = "./avatars/" + login + ".jpg";
+    // console.log(path);
+    // console.log(avatarUrl);
+    downloadImageByUrl(avatarUrl, path);
+  })
 });
+
+function downloadImageByUrl(url, filePath){
+  request.get(url)
+  .pipe(fs.createWriteStream(filePath));
+}
+
